cbbc5a00d1317220456d120b136162b289e2d897
polar <git@itzpolar.me>
2025-01-10 21:10:43 -0600

    feat: dairy implements command

--- dairy/src/main/java/dev/nullrobotics/choreolib/dairy/commands/ChoreoCommand.java
+++ dairy/src/main/java/dev/nullrobotics/choreolib/dairy/commands/ChoreoCommand.java
@@ -1,4 +1,43 @@
 package dev.nullrobotics.choreolib.dairy.commands;
 
-public class ChoreoCommand {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.Set;
+
+import dev.frozenmilk.dairy.core.wrapper.Wrapper;
+import dev.frozenmilk.mercurial.commands.Command;
+
+public class ChoreoCommand implements Command {
+    @Override
+    public void initialise() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean b) {
+
+    }
+
+    @Override
+    public boolean finished() {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public Set<Object> getRequirements() {
+        return Collections.emptySet();
+    }
+
+    @NotNull
+    @Override
+    public Set<Wrapper.OpModeState> getRunStates() {
+        return Collections.emptySet();
+    }
 }
